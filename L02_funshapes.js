edb32e87b9ae6aeabb4eca083c2d3373a635484f
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-18 10:14:08 +0000

    Jul 18, 2025, 10:14 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -26,7 +26,7 @@ function draw() {
   fill('black');
   ellipse(380,250,50,50);
   fill('black');
-  ellipse(250,330,50,50);
+  ellipse(350,330,50,50);
 
 
   // Challenge 3: A house using rect() function
