bf02475bf546c67cc88454001c9930bfb14918c4
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-18 10:59:54 +0000

    Jul 18, 2025, 10:59 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -43,6 +43,7 @@ function draw() {
   // fill('red');
   // triangle(80,200,720,200,400,10);
   //quad(123,456,789,234,567,890,345,678)
+  
   quad(1000,800,500,800,300,950,1300,950)
 
   // Challenge 4: A rocketship using triangle() function
