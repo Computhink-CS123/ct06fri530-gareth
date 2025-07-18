f9d992a8f5606ef41900b3198726bd6068d5d90e
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-18 10:20:07 +0000

    Jul 18, 2025, 10:20 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -32,7 +32,8 @@ function draw() {
   // Challenge 3: A house using rect() function
   fill('green');
   rect(200,150,400,300);
-  fill()
+  fill('blue')
+  
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
