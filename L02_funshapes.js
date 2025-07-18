86c0ca834b3193f768050ac57f14f5b9d7c18caa
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-18 10:23:47 +0000

    Jul 18, 2025, 10:23 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -37,7 +37,7 @@ function draw() {
   fill('blue')
   ellipse(490,300,70,70)
   fill('brown');
-  rect(300,300,70,100);
+  rect(400,200,70,100);
 
 
   // Challenge 4: A rocketship using triangle() function
