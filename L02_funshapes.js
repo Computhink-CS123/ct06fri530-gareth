d2d8a8300c1681feb36a124ad2e4df3455aab218
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-18 09:55:06 +0000

    Jul 18, 2025, 9:55 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ function setup(){
 function draw() {
   // Challenge 1: An eye using ellipse() function
   fill('black');
-  ellipse();
+  ellipse(400,300,);
 
   // Challenge 2: A face using circle() function
 
